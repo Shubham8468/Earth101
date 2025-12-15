9d55c8e1e74874efce0f4394570f788def9af51f
unknown <shubh386a@gmail.com>
2025-12-15 17:18:47 +0530

    active get start btn

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { PiClockAfternoon } from 'react-icons/pi'
 import { FaLongArrowAltRight } from "react-icons/fa";
 import About from './About';
+import { NavLink } from 'react-router-dom';
 const Home = () => {
   return (
     <main className='hero-section main'>
@@ -13,8 +14,9 @@ const Home = () => {
             <p className='paragraph'>
                 Discover detailed information about every country, from culture to geography, all in one place.
             </p>
+            <NavLink to="/country">
             <button className='btn btn-darken btn-inline bg-white-box'>Get Started <FaLongArrowAltRight /></button>
-
+            </NavLink>
           </div>
         <div className="hero-image" >
             <img src="./images/world.png" alt="world image" className='banner-img' />
